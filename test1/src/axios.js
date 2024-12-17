1b853aa21ed67464b8076e24f2f4fa8b6f0a6d57
H2suLee <h2su0712@naver.com>
2024-12-17 09:55:37 +0900

    관리자 내내 채팅

--- test1/src/axios.js
+++ test1/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9091", // 로컬 프론트에서 돌릴때
-  //baseURL: "http://localhost:9090", // 로컬 백엔드에서 돌릴때
+  //baseURL: "http://localhost:9091", // 로컬 프론트에서 돌릴때
+  baseURL: "http://localhost:9090", // 로컬 백엔드에서 돌릴때
   //baseURL: "https://oral-therine-heesu-f4935ad1.koyeb.app", // 백엔드 서버 주소
 });
 
