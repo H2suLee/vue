b86b2ee711f24ac8b3b2a9e0435f45415a6e50ee
H2suLee <h2su0712@naver.com>
2025-11-20 17:41:11 +0900

    test

--- test1/src/user/modules/components/login/LoginPage.vue
+++ test1/src/user/modules/components/login/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg">
     <div style="display: block">
-      <p>TOYCHAT_VUE</p>
+      <p>TOYCHAT</p>
       <a :href="getSocialLoginUrl('kakao')">
         <img src="@/assets/images/login/kakao_btn.webp" width="50"
       /></a>
